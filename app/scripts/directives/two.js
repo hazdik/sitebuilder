2419e7d3897908e2c2fa11a2380cbdfabcba922b
Matt Hippely <hippely@gmail.com>
2014-03-25 15:43:41 -0700

    Fixed vars for jslint.

--- app/scripts/directives/two.js
+++ app/scripts/directives/two.js
@@ -7,6 +7,9 @@ angular.module('pieologyApp')
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         element.text('this is the two directive');
+        console.log(attrs);
+        console.log(scope);
+        console.log(element);
       }
     };
   });
