7ea79a732924c75aff15b7b855eb13510d4ab9a5
Matt Hippely <hippely@gmail.com>
2014-03-28 15:09:18 -0700

    Added logic for global nav toggle.

--- app/scripts/directives/app-navigation.js
+++ app/scripts/directives/app-navigation.js
@@ -1,7 +1,7 @@
 /*global angular, console */
 
 angular.module('pieologyApp')
-    .directive('appNavigation', function () {
+    .directive('appNavigation', ['vlnConfig', function (vlnConfig) {
 
         'use strict';
 
@@ -11,9 +11,12 @@ angular.module('pieologyApp')
             replace    : true,
             link       : function postLink(scope, element, attrs) {
                 // element.text('this is the appNavigation directive');
+                // scope.isVisible = vlnConfig.getGlobalNavState();
+                // console.log('appNav: ', vlnConfig.getGlobalNavState());
+                // console.log('isVisible: ', scope.isVisible);
                 console.log(attrs);
                 console.log(scope);
                 console.log(element);
             }
         };
-    });
+    }]);
