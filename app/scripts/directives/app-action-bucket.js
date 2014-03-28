0e3284b63e858e3c23e585352cb691bb45ae63bb
Tsanko Tsolov <tsanko_tsolov@volusion.com>
2014-03-27 17:20:33 -0700

    APPAPP HEADER: Added iframe to "action-bucket"

--- app/scripts/directives/app-action-bucket.js
+++ app/scripts/directives/app-action-bucket.js
@@ -2,7 +2,7 @@
 /*global angular, console */
 
 angular.module('pieologyApp')
-    .directive('appActionBucket', function () {
+    .directive('appActionBucket', ['$rootScope', function ($rootScope) {
 
         'use strict';
 
@@ -10,10 +10,17 @@ angular.module('pieologyApp')
             templateUrl: 'views/action-bucket.html',
             restrict   : 'A',
             link       : function postLink(scope, element, attrs) {
-                // element.text('this is the appActionBucket directive');
-                console.log(attrs);
-                console.log(scope);
-                console.log(element);
+//                element.text('this is the appActionBucket directive');
+//                console.log(attrs);
+//                console.log(scope);
+//                console.log(element);
+
+                var displayClass = '';
+
+                $rootScope.$on('vlnDisplay.change', function (evt, params) {
+                    scope.displayClass = '-' + params.display;
+                });
+
             }
         };
-    });
+    }]);
--- app/scripts/directives/app-header.js
+++ app/scripts/directives/app-header.js
@@ -2,18 +2,23 @@
 /*global angular, console */
 
 angular.module('pieologyApp')
-    .directive('appHeader', function () {
+    .directive('appHeader', ['$rootScope', function ($rootScope) {
 
         'use strict';
 
         return {
             templateUrl: 'views/app-header.html',
             restrict   : 'A',
+//            scope      : {},
             link       : function postLink(scope, element, attrs) {
-                // element.html('<span class="icon-menu" onclick="alert(\'TEST\')"></span> ... this is the appHeader directive');
-                console.log(attrs);
-                console.log(scope);
-                console.log(element);
+//                element.html('<span class="icon-menu" onclick="alert(\'TEST\')"></span> ... this is the appHeader directive');
+//                console.log(attrs);
+//                console.log(scope);
+//                console.log(element);
+
+                scope.setDisplayTo = function (display) {
+                    $rootScope.$broadcast('vlnDisplay.change', { display : display });
+                };
             }
         };
-    });
+    }]);
