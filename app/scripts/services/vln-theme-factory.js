59a622ba8093450a93d18335ba61609c764a3f63
Matt Hippely <hippely@gmail.com>
2014-03-31 17:26:18 -0700

    Design Action: * Mocked data and images for themes. * Added simple style

--- app/scripts/services/vln-theme-factory.js
+++ app/scripts/services/vln-theme-factory.js
@@ -2,29 +2,36 @@
 
 angular.module('pieologyApp')
     .factory('vlnThemeFactory', ['$resource', '$q',
-            function($resource, $q) {
-                var themes = [{
+        function($resource, $q) {
+            var themes = [
+                {
                     'name': 'Theme 1',
-                    'url': 'https://www.google.com'
+                    'url': 'https://www.google.com',
+                    'thumbnail': 'http://lorempixel.com/200/200/'
                 }, {
                     'name': 'Theme 2',
-                    'url': 'https://www.yahoo.com'
+                    'url': 'https://www.yahoo.com',
+                    'thumbnail': 'http://lorempixel.com/200/200/'
                 }, {
                     'name': 'Theme 3',
-                    'url': 'https://www.bing.com'
+                    'url': 'https://www.bing.com',
+                    'thumbnail': 'http://lorempixel.com/200/200/'
                 }, {
                     'name': 'Theme 4',
-                    'url': 'https://www.duckduckgo.com'
-                }, ];
-
-                function getThemes() {
-                    var deferred = $q.defer();
-                    deferred.resolve(themes);
-                    return deferred.promise;
+                    'url': 'https://www.duckduckgo.com',
+                    'thumbnail': 'http://lorempixel.com/200/200/'
                 }
+            ];
 
-                // Public API here
-                return {
-                    getThemes: getThemes
-                };
-            });
+            function getThemes() {
+                var deferred = $q.defer();
+                deferred.resolve(themes);
+                return deferred.promise;
+            }
+
+            // Public API here
+            return {
+                getThemes: getThemes
+            };
+        }
+    ]);
