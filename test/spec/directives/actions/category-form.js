8fa49097b8c205876630459b7199548f49fe6e3c
Matt Hippely <hippely@gmail.com>
2014-04-07 13:39:04 -0700

    Page Action: * Added page-form & category form directives.

---
+++ test/spec/directives/actions/category-form.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Directive: actions/categoryForm', function () {
+
+  // load the directive's module
+  beforeEach(module('pieologyApp'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<actions/category-form></actions/category-form>');
+    element = $compile(element)(scope);
+    expect(element.text()).toBe('this is the actions/categoryForm directive');
+  }));
+});
---
+++ test/spec/directives/actions/page-form.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Directive: actions/pageForm', function () {
+
+  // load the directive's module
+  beforeEach(module('pieologyApp'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<actions/page-form></actions/page-form>');
+    element = $compile(element)(scope);
+    expect(element.text()).toBe('this is the actions/pageForm directive');
+  }));
+});
