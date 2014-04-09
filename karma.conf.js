e1b6f310df80b0b4a5da2f0a0da8b208db6b5d48
Matt Hippely <hippely@gmail.com>
2014-04-09 16:52:52 -0700

    Fixed grunt.

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function(config) {
             'app/bower_components/angular-animate/angular-animate.js',
             'app/bower_components/angular-bootstrap/ui-bootstrap.js',
             'app/bower_components/angular-touch/angular-touch.js',
-            'app/bower_components/angular-carousel/angular-carousel.js',
+            'app/bower_components/angular-carousel/dist/angular-carousel.js',
             'app/scripts/*.js',
             'app/scripts/**/*.js',
             'test/mock/**/*.js',
