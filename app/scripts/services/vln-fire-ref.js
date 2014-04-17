6ebd8074011cbecc337b56dcc8c19f2bd161871b
Tsanko Tsolov <tsanko_tsolov@volusion.com>
2014-04-16 17:56:44 -0700

    Basic theme switch (local)

--- app/scripts/services/vln-fire-ref.js
+++ app/scripts/services/vln-fire-ref.js
@@ -9,34 +9,70 @@ angular.module('pieologyApp')
             // var _products = {} || products // dont re init
 
             function productsFn() {
-                    /**
-                    @function
-                    @name productsFn
-                    @description return a firebase object initilaized with a list of products resource
-                    @param non
-                    @return Firebase reference object
+                /**
+                 @function
+                 @name productsFn
+                 @description return a firebase object initilaized with a list of products resource
+                 @param non
+                 @return Firebase reference object
                  */
-                 return $firebase( new Firebase(fbUrl + '/products') );
-             }
+                return $firebase(new Firebase(fbUrl + '/products'));
+            }
 
             function productFn(id) {
-                            /**
-                            @function
-                            @name productFn
-                            @description given a product id return a firebase reference to the data for that product
-                            @param {String} id
-                            @return Firebase reference object
-                         */
-                        return $firebase( new Firebase(fbUrl + '/products/'+id) );
+                /**
+                 @function
+                 @name productFn
+                 @description given a product id return a firebase reference to the data for that product
+                 @param {String} id
+                 @return Firebase reference object
+                 */
+                return $firebase(new Firebase(fbUrl + '/products/' + id));
             }
+
+            function themesFn() {
+                /**
+                 @function
+                 @name themesFn
+                 @description return a firebase object initilaized with a list of themes resource
+                 @param non
+                 @return Firebase reference object
+                 */
+                return $firebase(new Firebase(fbUrl + '/themes'));
+            }
+
+            function themeCurrentFn() {
+                /**
+                 @function
+                 @name themeCurrentFn
+                 @description return a firebase reference to the name for current theme
+                 @param {String} id
+                 @return Firebase reference object
+                 */
+                return $firebase(new Firebase(fbUrl + '/siteTheme'));
+            }
+
+            function themeFn(id) {
+                /**
+                 @function
+                 @name themeFn
+                 @description given a theme id return a firebase reference to the data for that theme
+                 @param {String} id
+                 @return Firebase reference object
+                 */
+                return $firebase(new Firebase(fbUrl + '/themes/' + id));
+            }
+
             // Public API here
             return {
-                products: productsFn,
-                product: productFn
-            }
+                products    : productsFn,
+                product     : productFn,
+                themes      : themesFn,
+                theme       : themeFn,
+                themeCurrent: themeCurrentFn
+            };
         }
-  ]);
-
+    ]);
 
 // angular.module('pieologyApp')
 //     .factory('vlnUpdateManager', ['$firebase', 'vlnConfig',
