ab07906253e9b24bd1727f95fbf269b40a6858d4
Matt Hippely <hippely@gmail.com>
2014-05-21 08:48:39 -0700

    Fix karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -65,6 +65,8 @@ module.exports = function (config) {
             'app/scripts/services/vln-update-manager.js',
             'app/scripts/services/vn-api.js',
             'app/scripts/services/vn-api-article-data.js',
+            'app/scripts/services/vn-api-category-data.js',
+            'app/scripts/services/vn-api-product-data.js',
             'app/scripts/services/vn-environment-context.js',
             'app/scripts/services/vn-data-endpoint.js',
             'app/scripts/services/vn-data-src.js',
